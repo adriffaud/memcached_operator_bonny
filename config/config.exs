09a4a387b546f17b9517a55554d9892800379b90
Adrien Driffaud <adrien@driffaud.fr>
2022-06-10 15:14:51 +0200

    Watch all namespaces

--- config/config.exs
+++ config/config.exs
@@ -8,6 +8,8 @@ config :bonny,
   get_conn: {K8s.Conn, :from_file, ["~/.kube/config", [context: "minikube"]]},
   group: "cache.driffaud.fr",
   operator_name: "memcached-operator",
-  api_version: "apiextensions.k8s.io/v1"
+  api_version: "apiextensions.k8s.io/v1",
+  # Will only watch its own namespace if not specified, unlike specified in the docs
+  namespace: ""
 
 import_config("#{config_env()}.exs")
